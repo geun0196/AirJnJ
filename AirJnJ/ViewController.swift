2d24e9339045f738a38756636872be74dfd48bce
geun0196 <73518321+geun0196@users.noreply.github.com>
2022-01-27 20:18:50 +0900

    Initial Commit

---
+++ AirJnJ/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AirJnJ
+//
+//  Created by usg on 2022/01/27.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
